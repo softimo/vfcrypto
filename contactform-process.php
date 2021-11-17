c761712f29c6ca55ca9a8d307ec3a0c38bed72f7
Cynbertea <cynbertea0764@gmail.com>
2021-11-17 17:57:06 -0300

    cambios guille

---
+++ contactform-process.php
@@ -0,0 +1,55 @@
+<?php
+$errorMSG = "";
+
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+if (empty($_POST["message"])) {
+    $errorMSG = "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+
+$EmailTo = "contacto@softimo.com.ar";
+$Subject = "Nuevo mensaje de contacto";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+$header = "From: contacto@softimo.com.ar\nReply-To:".$_POST["email"]."\n";
+$header .= "Mime-Version: 1.0\n";
+$header .= "Content-Type: text/plain";
+// send email
+$success = mail($EmailTo, $Subject, $Body, $header);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
